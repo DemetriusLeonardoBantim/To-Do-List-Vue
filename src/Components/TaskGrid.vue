50775c71f00ad880addb08f68abcd18111a7e767
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-01-28 15:27:06 -0300

    v1 to-do list

---
+++ src/Components/TaskGrid.vue
@@ -0,0 +1,68 @@
+<template>
+   <div class="task-grid">
+    <template v-if="tasks.length">
+     <Task v-for="(task,i) in tasks" :key="task.name" 
+     @taskDeleted="$emit('taskDeleted', i)"
+     @taskStateChanged="$emit('taskStateChanged', i)"
+     :task="task">
+     </Task>
+    </template>
+    <p  v-else class="no-task">Sua vida esta em dia =)</p>
+   </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+ components: {Task},
+ props : {
+  tasks: {type: Array, required: true}
+ }
+}
+</script>
+
+<style>
+ .task{
+  box-sizing: border-box;
+  width: 350px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px ;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  user-select:none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+ }
+
+.pending{
+ border-left: 12px solid #B73229;
+ background-color: #F44336;
+}
+
+.done{
+ color: #DDD;
+ border-left: 12px solid #0A8F08;
+ background-color: #4CAF50;
+ text-decoration: line-through;
+}
+
+.task-grid{
+ display: flex;
+ justify-content: center;
+ flex-wrap: wrap;
+}
+
+.task-grid .task {
+ margin: 10px;
+}
+
+.no-task{
+ color: #AAA;
+ font-size: 1.7rem;
+}
+
+</style>
